86573c1055847af98fca2f65dc0881900a9ad627
Flavio Oliveira <flavio.m@samsung.com>
2018-12-03 15:18:23 -0200

    Accessing javascript variables inside JSX blocks

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-  return 'Click on me!';
-}
 
 // Create a react component
 const App = () => {
 
+  const buttonText = {text: 'Clicke Me!'};
+  const labelText = 'Enter name:';
   return (
     <div>
-      <label className="label" for="name">
-        Enter name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {getButtonText()}
+        {buttonText.text}
       </button>
     </div>
   );

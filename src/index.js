63e9fb0ee78f86dca085f4840ed82b4a076594a7
Flavio Oliveira <flavio.m@samsung.com>
2018-12-03 10:26:02 -0200

    Exploring more JSX

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click on me!';
+}
+
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 
